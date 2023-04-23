e81691c5d301d7395b96acbfad1abe3e02f23bf1
JoelJa835 <joeljani71@gmail.com>
2023-04-23 14:54:31 +0300

    i fixed the fact that i was using np.max instead of min to calculate the regret and also modified the bandit function because i wasnt using the correct loss... i wasnt includin the propability.. as we should on the adversarial bandit implementation.

--- server_load_prediction.py
+++ server_load_prediction.py
@@ -11,17 +11,14 @@ def mw_expert(dataset_path, T):
     n_servers = data.shape[1]
     weights = np.ones(n_servers)
     regrets = []
+    eta=np.sqrt(np.log(n_servers)/T)
     for t in range(T):
-        if t == 0:
-            eta= np.sqrt(np.log(n_servers))
-        else:
-            eta=np.sqrt(np.log(n_servers)/t)
         # pick a server with probability proportional to the weights
         choice = np.random.choice(n_servers, p=weights/np.sum(weights))
         # observe the loss of all servers
         losses = data.iloc[t,:].values
         # calculate the regret
-        regret = np.max(losses) - losses[choice]
+        regret = losses[choice] -np.min(losses)
         regrets.append(regret)
         # update the weights of all servers
         weights = weights * np.exp(-eta* losses)
@@ -30,30 +27,29 @@ def mw_expert(dataset_path, T):
 
 
 def mw_adversarial(dataset_path, T):
-    data = pd.read_csv(dataset_path,header=None)
+    data = pd.read_csv(dataset_path, header=None)
     data = data.T
     n_servers = data.shape[1]
     weights = np.ones(n_servers)
     regrets = []
+    eta = np.sqrt(np.log(n_servers) / T)
     for t in range(T):
-        if t == 0:
-            eta= np.sqrt(np.log(n_servers))
-            epsilon = 1
-        else:
-            eta= np.sqrt(np.log(n_servers)/t* n_servers)
-            epsilon = np.sqrt(np.log(n_servers) / (t * n_servers))
-        # pick a server with probability epsilon, and otherwise with probability proportional to the weights
-        if np.random.random() < epsilon:
-            choice = np.random.choice(n_servers)
-        else:
-            choice = np.random.choice(n_servers, p=weights/np.sum(weights))
-        # observe the loss of the chosen server
-        loss = data.iloc[t,choice]
+        # calculate the probability of selecting each action i
+        pi = weights / np.sum(weights)
+        # select an action i with probability proportional to pi
+        choice = np.random.choice(n_servers, p=pi)
+        # observe the loss of the chosen action
+        loss = data.iloc[t, choice]
         # calculate the regret
-        regret = np.max(data.iloc[t,:].values) - loss
-        regrets.append(regret)
-        # update the weights of the chosen server using the MW update rule
-        weights *= np.exp(-eta * loss)
+        regret = np.min(data.iloc[t, :].values) - loss
+        regrets.append(np.abs(regret))
+        # update the weights of all actions using the MW update rule
+        for i in range(n_servers):
+            if i == choice:
+                weights[i] *= np.exp(-eta * loss / pi[i])
+            else:
+                weights[i] *= np.exp(eta * loss / ((1 - pi[choice]) * (n_servers - 1)))
+                
         # renormalize the weights to ensure they sum to 1
         weights /= np.sum(weights)
     return np.cumsum(regrets)
@@ -71,11 +67,11 @@ for t in T:
     #regrets_ucb = UCB(t)
 
     plt.figure(figsize=(8, 6))
-    # plt.plot(regrets_expert, label='MW Expert')
-    # plt.plot(regrets_bandit, label='MW Bandit')
+    plt.plot(regrets_expert, label='MW Expert')
+    plt.plot(regrets_bandit, label='MW Bandit')
     #plt.plot(regrets_ucb, label='UCB-Bandit')
-    plt.semilogy(regrets_expert, label='MW Expert')
-    plt.semilogy(regrets_bandit, label='MW Bandit')
+    # plt.semilogy(regrets_expert, label='MW Expert')
+    # plt.semilogy(regrets_bandit, label='MW Bandit')
     plt.legend()
     plt.title(f"T={t}")
     plt.xlabel('Time')
